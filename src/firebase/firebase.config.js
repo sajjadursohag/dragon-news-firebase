64acad6302c470b336a8a91c5b5836f447fc9a75
sajjadursohag <sajjadursub07@gmail.com>
2025-10-20 23:39:01 +0600

    added login and register page

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_InvPweB4JbMROK2xM6aowP8f9a1m7Zs",
+  authDomain: "dragon-breaking-news-68fac.firebaseapp.com",
+  projectId: "dragon-breaking-news-68fac",
+  storageBucket: "dragon-breaking-news-68fac.firebasestorage.app",
+  messagingSenderId: "264400931061",
+  appId: "1:264400931061:web:ad54ab2078797acb2a1dbc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
